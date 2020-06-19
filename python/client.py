a8af429be9974dfaa10936681ca0a90a759712c0
JonathanSchmalhofer <jonathan.schmalhofer@gmail.com>
2020-06-19 22:38:37 +0200

    adjust client call

--- python/client.py
+++ python/client.py
@@ -4,29 +4,31 @@ import socket
 import time
 
 def DoEpisode():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.settimeout(1) # 1 second
-	s.connect((TCP_IP, TCP_PORT))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1) # 1 second
+    s.connect((TCP_IP, TCP_PORT))
 
-	s.send("(HEAD:10)(REGISTER)".encode('utf-8'))
-	for i in range(0, 5):
-		s.send("(HEAD:22)(CONTROL:0.3;0.1;-0.1)".encode('utf-8'))
-		data = s.recv(BUFFER_SIZE)
-	for i in range(0, 5):
-		s.send("(HEAD:22)(CONTROL:0.6;0.15;0.4)".encode('utf-8'))
-		data = s.recv(BUFFER_SIZE)
-	s.send("(HEAD:7)(CLOSE)".encode('utf-8'))
-	s.close()
-	s = None
+    #print("   REGISTER");
+    s.send("(HEAD:10)(REGISTER)".encode('utf-8'))
+    data = s.recv(BUFFER_SIZE)
+    print("   RESPONSE: {}".format(data))
+    for i in range(0, 100):
+        s.send("(HEAD:22)(CONTROL:0.3;0.1;-0.1)".encode('utf-8'))
+        data = s.recv(BUFFER_SIZE)
+        #print("   RESPONSE: {}".format(data))
+    for i in range(0, 100):
+        s.send("(HEAD:22)(CONTROL:0.6;0.15;0.4)".encode('utf-8'))
+        data = s.recv(BUFFER_SIZE)
+        #print("   RESPONSE: {}".format(data))
+    s.send("(HEAD:7)(CLOSE)".encode('utf-8'))
+    s.close()
+    s = None
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 42424
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
 
-print("Episode 1")
-DoEpisode()
-print("Episode 2")
-DoEpisode()
-print("Episode 3")
-DoEpisode()
+for i in range(0, 100):
+    print("Episode {}".format(i))
+    DoEpisode()
